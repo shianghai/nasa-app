9093467f7adecc6570b506efac99ee6d61e89c40
shianghai <shiangahfrancis@outlook.com>
2023-03-05 11:31:44 +0000

    load planets data in test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,11 +1,12 @@
 const request  = require('supertest');
 const app  = require("../../app");
 const {connectToMongo, disconnectFromMongo} = require('../../services/mongo');
-
+const {loadPlanetsData} = require('../../models/planets.model'); 
 
 describe("Launches API", ()=>{
     beforeAll(async()=>{
         await connectToMongo();
+        await loadPlanetsData();
     });
 
     describe("Test Get /launches", ()=>{
